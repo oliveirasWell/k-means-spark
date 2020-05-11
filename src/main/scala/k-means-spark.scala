5feda8b9127a51fbff7bae3c25ac1221a7dd6ba5
Wellington Oliveira <well.oliveira.snt@gmail.com>
2020-05-11 04:17:49 -0300

    Initial commit

---
+++ src/main/scala/k-means-spark.scala
@@ -0,0 +1,9 @@
+object hello {
+  def main(args: Array[String]) {
+    print("\n\n>>>>> START OF PROGRAM <<<<<\n\n");
+
+    println("Hello World.")
+
+    print("\n\n>>>>> END OF PROGRAM <<<<<\n\n");
+  }
+}
